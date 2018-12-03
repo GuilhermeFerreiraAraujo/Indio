a45ddd4f8548ab14963e197f0d3bb7964fa8d1f0
Guilherme <gui_s3@hotmail.com>
2018-12-03 19:50:49 +0100

    Add router

---
+++ Indio/wwwroot/aurelia-app/src/app.ts
@@ -0,0 +1,28 @@
+ import {RouterConfiguration, Router} from 'aurelia-router';
+
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+ import { PLATFORM } from "aurelia-framework";
+
+  export class App {
+    router: Router;
+    message = "Hello World!";
+
+    activated() {
+      console.log(this.router);
+    }
+
+    configureRouter(config: RouterConfiguration, router: Router): void {
+      
+      config.title = 'Aurelia';
+      config.map([
+        { route: ['', 'accounts'],  title: "Accounts", nav:true,    name: 'Accounts',       moduleId: PLATFORM.moduleName('views/accounts') },
+        { route: ['customers'],    title: "Customers", nav:true,     name: 'Customers',       moduleId: PLATFORM.moduleName('views/customers') },
+
+     
+      ]);
+      this.router = router;
+
+    }
+  }
+  
--- Indio/wwwroot/jspm_packages/npm/aurelia-bootstrapper@2.3.0.js
+++
@@ -1,3 +0,0 @@
-define(["npm:aurelia-bootstrapper@2.3.0/aurelia-bootstrapper"], function(main) {
-  return main;
-});
