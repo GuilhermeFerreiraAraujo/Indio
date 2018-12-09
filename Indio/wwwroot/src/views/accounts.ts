ecb1585af4f3d3689261e9d9db4641459e584476
Guilherme <gui_s3@hotmail.com>
2018-12-09 19:03:26 +0100

    Display account content on table

--- Indio/wwwroot/src/views/accounts.ts
+++ Indio/wwwroot/src/views/accounts.ts
@@ -1,8 +1,11 @@
 
 import {HttpClient} from 'aurelia-http-client';
+import { AccountModel } from 'models/account.models';
 
 
 export class accounts {
+
+    items: AccountModel[] = [];
 
     activate() {
        
@@ -10,7 +13,7 @@ export class accounts {
         
         client.get('https://localhost:44307/api/accounts/get')
           .then(data => {
-            console.log(data)
+            this.items = JSON.parse(data.response);
           }).catch(ex => {
               console.log(ex);
             
